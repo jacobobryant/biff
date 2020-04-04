88400f0790726a5e96e7ec6f73a19ab071358c46
Jacob O'Bryant <foo@jacobobryant.com>
2020-04-04 17:42:15 -0600

    Add install script

---
+++ install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -e
+
+apt update
+apt upgrade
+add-apt-repository ppa:certbot/certbot
+apt install nginx python-certbot-nginx default-jre
+curl -O https://download.clojure.org/install/linux-install-1.10.1.536.sh
+chmod +x linux-install-1.10.1.536.sh
+./linux-install-1.10.1.536.sh
+
+cat > /etc/systemd/system/biff.service << EOD
+[Unit]
+Description=Biff
+
+[Service]
+ExecStart=/root/biff/template/start-biff.sh
+
+[Install]
+WantedBy=multi-user.target
+EOD
+systemctl enable biff
+
+rm /etc/nginx/sites-enabled/default
+cat > /etc/nginx/sites-available/biff << EOD
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    server_name _;
+    root /var/www/site;
+    location / {
+        try_files $uri $uri/ @proxy;
+    }
+    location @proxy {
+        proxy_pass http://localhost:8080/;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+    }
+}
+EOD
+ln -s /etc/nginx/sites-{available,enabled}/biff
+systemctl restart nginx
+
+echo
+echo Running certbot now. When it asks if you\'d like to redirect HTTP requests to HTTPS, say yes.
+read -p "Press Enter to continue"
+certbot --nginx
+systemctl restart nginx
+
+ufw allow "Nginx Full"
+ufw allow OpenSSH
+ufw enable
+
+echo Installation complete. To start Biff, run \`reboot\` and then wait for a minute.
+echo The default admin password for Biff is: hey
+echo Be sure to change it after you log in.
--- template/start-biff.sh
+++ template/start-biff.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-while clj -m biff.core; do sleep 1; done
+cd "$(dirname "${BASH_SOURCE[0]}")"
+while clojure -m biff.core; do sleep 1; done
