b19fd0912e84e2521b2da5c1e8971906abc0d46f
Jacob O'Bryant <foo@jacobobryant.com>
2020-06-11 23:34:09 -0600

    Remove old file

--- resources/public/biff/pack/js/restart.js
+++
@@ -1,8 +0,0 @@
-function check() {
-  fetch("/biff/pack/ping").then(r => {
-    if (r.ok) {
-      window.location = "/biff/pack";
-    }
-  });
-}
-setTimeout(() => setInterval(check, 1000), 3000);
