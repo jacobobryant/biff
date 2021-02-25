bf2805df5718201a46d1c3c9bd2069a2e8fe5ae1
Jeff Parker <jeffp42ker@gmail.com>
2021-02-25 10:16:18 -0500

    Test clj repl-aliases support to determine exec-opts

--- new-project.sh
+++ new-project.sh
@@ -1,3 +1,5 @@
 sha="$(git ls-remote https://github.com/jacobobryant/biff.git HEAD | awk '{ print $1 }')"
 deps="{:deps {github-jacobobryant/biff {:git/url \"https://github.com/jacobobryant/biff\" :sha \"$sha\"}}}"
-clj -Sdeps "$deps" -M -m biff.project
+chkapi=`clj -Sdescribe | grep repl-aliases`
+mopt="" && [[ -n $chkapi ]] && mopt="-M"
+clj -Sdeps "$deps" "$mopt" -m biff.project
