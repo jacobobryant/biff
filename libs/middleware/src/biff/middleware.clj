16cdf15bc39d82c5d87c3244634adf04727ecd3e
Jacob O'Bryant <git@jacobobryant.com>
2021-04-28 15:03:06 -0700

    Add biff.middleware/wrap-static

--- libs/middleware/src/biff/middleware.clj
+++ libs/middleware/src/biff/middleware.clj
@@ -1,9 +1,36 @@
 (ns biff.middleware
-  (:require
-    [clojure.stacktrace :as st]
-    [biff.util-tmp :as bu]
-    [muuntaja.middleware :as muuntaja]
-    [ring.middleware.defaults :as rd]))
+  (:require [clojure.java.io :as io]
+            [clojure.stacktrace :as st]
+            [clojure.string :as str]
+            [biff.util-tmp :as bu]
+            [muuntaja.middleware :as muuntaja]
+            [ring.middleware.defaults :as rd]
+            [ring.util.codec :as codec]
+            [ring.util.io :as rio]
+            [ring.util.request :as request]
+            [ring.util.time :as rtime]))
+
+(defn wrap-static [handler {:keys [root path->file]
+                            :or {root "public"
+                                 path->file (comp io/file io/resource)}}]
+  (fn [{:keys [request-method] :as req}]
+    (or (when (#{:get :head} request-method)
+          (let [path (str root (codec/url-decode (request/path-info req)))
+                file (path->file path)
+                file (if (some-> file (.isDirectory))
+                       (path->file (str/replace-first path #"/?$" "/index.html"))
+                       file)]
+            (when (some-> file (.isFile))
+              (bu/nest-string-keys
+                (merge {:status 200
+                        :headers/Content-Length (str (.length file))
+                        :headers/Last-Modified (rtime/format-date (rio/last-modified-date file))}
+                       (when (not= :head request-method)
+                         {:body file})
+                       (when (str/ends-with? (.getPath file) ".html")
+                         {:headers/Content-Type "text/html"}))
+                [:headers]))))
+        (handler req))))
 
 (defn wrap-flat-keys [handler]
   (fn [{:keys [session params] :as req}]
@@ -49,5 +76,6 @@
       wrap-flat-keys
       muuntaja/wrap-params
       muuntaja/wrap-format
+      (wrap-static (select-keys opts [:root :path->file]))
       (rd/wrap-defaults ring-defaults)
       (wrap-internal-error {:on-error on-error}))))
