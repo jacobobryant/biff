5f0dbaa6bdd99ce6b696acd9483a5d985143039f
Jacob O'Bryant <git@jacobobryant.com>
2021-04-28 15:02:21 -0700

    Add :biff.reitit/on-error

--- libs/components/src/biff/components_tmp.clj
+++ libs/components/src/biff/components_tmp.clj
@@ -16,14 +16,24 @@
       (println "nrepl running on port" port)))
   sys)
 
-(defn reitit [{:biff.reitit/keys [routes default-handlers]
+(defn reitit [{:biff.reitit/keys [routes default-handlers on-error]
                :as sys}]
-  (let [router (reitit/router routes)]
+  (let [router (reitit/router routes)
+        default-handlers (when on-error
+                           (concat default-handlers
+                                   [(reitit/create-default-handler
+                                      (->> [:not-found :method-not-allowed :not-acceptable]
+                                           (map (fn [k]
+                                                  [k #(on-error (assoc % :reitit-error k))]))
+                                           (into {})))]))
+        handler (if (not-empty default-handlers)
+                  (reitit/ring-handler
+                    router
+                    (apply reitit/routes default-handlers))
+                  (reitit/ring-handler router))]
     (assoc sys
            :biff.reitit/router router
-           :biff.web/handler (reitit/ring-handler
-                               router
-                               (apply reitit/routes default-handlers)))))
+           :biff.web/handler handler)))
 
 (defn jetty [{:biff.web/keys [host port handler]
               :biff.jetty/keys [quiet websockets]
