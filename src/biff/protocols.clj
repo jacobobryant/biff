42898f615b9b945d06348d9997ca1e5ecd0448e6
Jacob O'Bryant <git@jacobobryant.com>
2021-05-08 17:51:55 -0700

    Delete old biff code

--- src/biff/protocols.clj
+++
@@ -1,9 +0,0 @@
-(ns biff.protocols)
-
-(defprotocol DbClient
-  (get-batch! [client env])
-  (get-id->before+after [client batch])
-  (query-contains-doc? [client query doc])
-  (batch->auth-fn-opts [client batch])
-  (get-trigger-data [client env batch])
-  (doc->id+doc [client doc]))
