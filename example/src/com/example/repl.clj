9b6c8e8a087a1a5414dac24d3844f862a5c46335
Jacob O'Bryant <git@jacobobryant.com>
2022-12-09 16:59:19 -0800

    Add fixtures to the example app

--- example/src/com/example/repl.clj
+++ example/src/com/example/repl.clj
@@ -1,8 +1,16 @@
 (ns com.example.repl
-  (:require [com.biffweb :as biff :refer [q]]))
+  (:require [com.biffweb :as biff :refer [q]]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]))
 
 (defn get-sys []
   (biff/assoc-db @biff/system))
+
+(defn add-fixtures []
+  (biff/submit-tx (get-sys)
+    (-> (io/resource "fixtures.edn")
+        slurp
+        edn/read-string)))
 
 (comment
 
@@ -10,6 +18,12 @@
   ;; start going to Vim. fix-print makes sure stdout keeps going to the
   ;; terminal. It may not be necessary in your editor.
   (biff/fix-print (biff/refresh))
+
+  ;; Call this in dev if you'd like to add some seed data to your database. If
+  ;; you edit the seed data (in resources/fixtures.edn), you can reset the
+  ;; database by running `rm -r storage/xtdb` (DON'T run that in prod),
+  ;; restarting your app, and calling add-fixtures again.
+  (add-fixtures)
 
   (let [{:keys [biff/db] :as sys} (get-sys)]
     (q db
