49ce399056e469a76848a2b29ec839004623134b
Jeff Terrell <terrell@cs.unc.edu>
2022-12-24 17:31:39 -0500

    Fix an execution error when starting a prod server
    I was getting errors like these from `bb logs`:
    
    ```
    Dec 24 20:57:06 biff-tutorial-eelchat sh[2518]: Execution error at nrepl.cmdline/parse-cli-values$fn (cmdline.clj:264).
    Dec 24 20:57:06 biff-tutorial-eelchat sh[2518]: EOF while reading
    Dec 24 20:57:06 biff-tutorial-eelchat sh[2518]: Full report at:
    Dec 24 20:57:06 biff-tutorial-eelchat sh[2518]: /tmp/clojure-2833585522771129812.edn
    ```

--- tasks/src/com/biffweb/tasks.clj
+++ tasks/src/com/biffweb/tasks.clj
@@ -58,7 +58,7 @@
   (io/make-parents "target/resources/_")
   (when (fs/exists? "package.json")
     (shell "npm" "install"))
-  (apply println "clj" (run-args)))
+  (apply shell "clj" (run-args)))
 
 (defn css [& args]
   (apply shell
