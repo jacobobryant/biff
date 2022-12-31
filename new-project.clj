aa71c61a4b90a11db32ba3bf7a094155a8a8caa4
Jacob O'Bryant <git@jacobobryant.com>
2022-12-30 20:16:20 -0800

    Set :tag in deps.edn for new projects

--- new-project.clj
+++ new-project.clj
@@ -5,6 +5,8 @@
             [clojure.string :as str])
   (:import java.security.SecureRandom))
 
+(def repo-url "https://github.com/jacobobryant/biff")
+
 (def java-major-version
   (when (fs/which "javap")
     (->> (shell/sh "javap" "-verbose" "java.lang.String")
@@ -41,23 +43,43 @@
     (.nextBytes (SecureRandom.) buffer)
     (.encodeToString (java.util.Base64/getEncoder) buffer)))
 
-(defn biff-coordinates [sha]
-  (str ":git/url \"https://github.com/jacobobryant/biff\" :sha \"" sha "\""))
+(defn fetch-refs []
+  (-> (sh "git" "ls-remote" (str repo-url ".git"))
+      (str/split #"\s+")
+      (->> (partition 2)
+           (map (comp vec reverse))
+           (into {}))))
+
+(defn die [exit-code & message]
+  (binding [*out* *err*]
+    (apply println message)
+    (System/exit exit-code)))
 
 (defn -main [& [branch]]
   (when (nil? java-major-version)
-    (println "`java` command not found. Please install Java 11 or higher.")
-    (System/exit 1))
+    (die 1 "`java` command not found. Please install Java 11 or higher."))
   (when (< java-major-version 55)
-    (println "Please install Java 11 or higher.")
-    (System/exit 2))
+    (die 2 "Please install Java 11 or higher."))
   (when-not (fs/which "curl")
-    (println "`curl` command not found. Please install it. (`scoop install curl` on Windows.)")
-    (System/exit 3))
-  (let [sha (-> (sh "git" "ls-remote" "https://github.com/jacobobryant/biff.git" (or branch "HEAD"))
-                (str/split #"\s+")
-                first)
-        coordinates (biff-coordinates sha)
+    (die 3 "`curl` command not found. Please install it. (`scoop install curl` on Windows.)"))
+  (let [ref->commit (fetch-refs)
+        commit (if-not branch
+                 (ref->commit "HEAD")
+                 (ref->commit (str "refs/heads/" branch)))
+        _ (when-not commit
+            (die 4 "Invalid git branch:" branch))
+        tag (some-> (filter (fn [[ref_ commit_]]
+                              (and (= commit commit_)
+                                   (str/starts-with? ref_ "refs/tags/v")))
+                            ref->commit)
+                    ffirst
+                    (str/replace "refs/tags/" ""))
+        coordinates (if tag
+                      {:git/url repo-url
+                       :sha (subs commit 0 7)
+                       :tag tag}
+                      {:git/url repo-url
+                       :sha commit})
         cookie-secret (new-secret 16)
         jwt-secret (new-secret 32)
         dir (->> (prompt "Enter name for project directory: ")
@@ -69,8 +91,8 @@
         example (io/file tmp "biff" "example")]
     (io/make-parents (io/file tmp "_"))
     (if branch
-      (sh "git" "clone" "--single-branch" "--branch" branch "https://github.com/jacobobryant/biff" :dir tmp)
-      (sh "git" "clone" "https://github.com/jacobobryant/biff" :dir tmp))
+      (sh "git" "clone" "--single-branch" "--branch" branch repo-url :dir tmp)
+      (sh "git" "clone" repo-url :dir tmp))
     (doseq [src (->> (file-seq example)
                      (filter #(.isFile %)))
             :let [relative (-> (.getPath src)
@@ -85,8 +107,9 @@
                 (str/replace "com.example" main-ns)
                 (str/replace #"cookie-secret nil" (str "cookie-secret " (pr-str cookie-secret)))
                 (str/replace #"jwt-secret nil" (str "jwt-secret " (pr-str jwt-secret)))
-                (str/replace ":local/root \"..\"" coordinates)
-                (str/replace ":local/root \"../../tasks\"" (str coordinates " :deps/root \"tasks\"")))))
+                (str/replace "{:local/root \"..\"}" (pr-str coordinates))
+                (str/replace "{:local/root \"../../tasks\"}"
+                             (pr-str (assoc coordinates :deps/root "tasks"))))))
     (.renameTo (io/file dir "config.edn.TEMPLATE") (io/file dir "config.edn"))
     (rmrf tmp)
     (sh "bb" "--force" "-e" "nil" :dir dir)
