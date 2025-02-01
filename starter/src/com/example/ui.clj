5316f343139a5572788e70ba9bc39f039ff615e3
aleksei.zolotykh <aazolotyh@gmail.com>
2025-02-01 15:16:53 +0300

    Update versions htmx, hyperscript and extensions

--- starter/src/com/example/ui.clj
+++ starter/src/com/example/ui.clj
@@ -27,9 +27,9 @@
        (update :base/head (fn [head]
                             (concat [[:link {:rel "stylesheet" :href (static-path "/css/main.css")}]
                                      [:script {:src (static-path "/js/main.js")}]
-                                     [:script {:src "https://unpkg.com/htmx.org@1.9.12"}]
-                                     [:script {:src "https://unpkg.com/htmx.org@1.9.12/dist/ext/ws.js"}]
-                                     [:script {:src "https://unpkg.com/hyperscript.org@0.9.8"}]
+                                     [:script {:src "https://unpkg.com/htmx.org@2.0.4"}]
+                                     [:script {:src "https://unpkg.com/htmx-ext-ws@2.0.1/ws.js"}]
+                                     [:script {:src "https://unpkg.com/hyperscript.org@0.9.13"}]
                                      (when recaptcha
                                        [:script {:src "https://www.google.com/recaptcha/api.js"
                                                  :async "async" :defer "defer"}])]
