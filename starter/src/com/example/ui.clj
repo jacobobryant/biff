24c73fc342b2cd14af1b534b42e0fa2441fc059d
Jacob O'Bryant <hello@obryant.dev>
2025-09-14 05:53:25 -0600

    Update default frontend dependencies

--- starter/src/com/example/ui.clj
+++ starter/src/com/example/ui.clj
@@ -27,9 +27,9 @@
        (update :base/head (fn [head]
                             (concat [[:link {:rel "stylesheet" :href (static-path "/css/main.css")}]
                                      [:script {:src (static-path "/js/main.js")}]
-                                     [:script {:src "https://unpkg.com/htmx.org@2.0.4"}]
-                                     [:script {:src "https://unpkg.com/htmx-ext-ws@2.0.1/ws.js"}]
-                                     [:script {:src "https://unpkg.com/hyperscript.org@0.9.13"}]
+                                     [:script {:src "https://unpkg.com/htmx.org@2.0.7"}]
+                                     [:script {:src "https://unpkg.com/htmx-ext-ws@2.0.2/ws.js"}]
+                                     [:script {:src "https://unpkg.com/hyperscript.org@0.9.14"}]
                                      (when recaptcha
                                        [:script {:src "https://www.google.com/recaptcha/api.js"
                                                  :async "async" :defer "defer"}])]
